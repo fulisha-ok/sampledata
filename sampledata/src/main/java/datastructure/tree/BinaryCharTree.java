a2ba7385a7ed614cd443e15a73e2d6f1cff37fd4
fulisha <2695434642@qq.com>
2023-04-10 14:49:45 +0800

    day24

--- sampledata/src/main/java/datastructure/tree/BinaryCharTree.java
+++ sampledata/src/main/java/datastructure/tree/BinaryCharTree.java
@@ -76,6 +76,42 @@ public class BinaryCharTree {
     }
 
     /**
+     * The parameters must be correct since no validity.check is undertaken.
+     * @param paraDataArray  The array for data.
+     * @param paraIndicesArray The array for indices.
+     */
+    public BinaryCharTree(char[] paraDataArray, int[] paraIndicesArray){
+        // Step 1. Use a sequential list to store all nodes.
+        int tempNumNodes = paraDataArray.length;
+        BinaryCharTree[] tempAllNodes = new BinaryCharTree[tempNumNodes];
+        for (int i = 0; i < tempNumNodes; i++){
+            tempAllNodes[i] = new BinaryCharTree(paraDataArray[i]);
+        }
+
+        for (int i = 1; i < tempNumNodes; i++){
+            for (int j = 0; j < i; j++){
+                System.out.println("indices " + paraIndicesArray[j] + " vs. " + paraIndicesArray[i]);
+                if (paraIndicesArray[i] == paraIndicesArray[j]*2 + 1){
+                    // i结点是j结点的左孩子
+                    tempAllNodes[j].leftChild = tempAllNodes[i];
+                    System.out.println("Linking " + j + " with " + i);
+                    break;
+                }else if (paraIndicesArray[i] == paraIndicesArray[j] * 2 + 2){
+                    // i结点是j结点的右孩子
+                    tempAllNodes[j].rightChild = tempAllNodes[i];
+                    System.out.println("Linking " + j + " with " + i);
+                    break;
+                }
+            }
+        }
+
+        //Step 3. The root is the first node.
+        value = tempAllNodes[0].value;
+        leftChild = tempAllNodes[0].leftChild;
+        rightChild = tempAllNodes[0].rightChild;
+    }
+
+    /**
      * pre-order visit
      */
     public void  preOrderVisit(){
@@ -294,6 +330,20 @@ public class BinaryCharTree {
         System.out.println("Only object queue.");
         System.out.println("The values are: " + Arrays.toString(tempTree.valuesArray));
         System.out.println("The indices are: " + Arrays.toString(tempTree.indicesArray));
+
+        System.out.println("*********************************************");
+        //char[] tempCharArray = {'A', 'B', 'C', 'D', 'E', 'F'};
+        //int[] tempIndicesArray = {0, 1, 2, 4, 5, 12};
+        char[] tempCharArray = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+        int[] tempIndicesArray = {0, 1, 2, 4, 5, 9, 10};
+        BinaryCharTree tempTree2 = new BinaryCharTree(tempCharArray, tempIndicesArray);
+
+        System.out.println("\r\nPreorder visit:");
+        tempTree2.preOrderVisit();
+        System.out.println("\r\nIn-order visit:");
+        tempTree2.inOrderVisit();
+        System.out.println("\r\nPost-order visit:");
+        tempTree2.postOrderVisit();
     }
 
 
